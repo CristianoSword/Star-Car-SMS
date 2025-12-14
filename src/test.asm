8fc7ebd6c03820c83bafb5de576afa978ce41bcb
Cristiano Camacho <originalvgo@gmail.com>
2025-12-14 16:11:50 -0300

    Create src/test.asm with memory and bank setup
    Add assembly source file with memory and bank mapping.

---
+++ src/test.asm
@@ -0,0 +1,26 @@
+.memorymap
+defaultslot 0
+slotsize $8000
+slot 0 $0000
+.endme
+
+.rombankmap
+bankstotal 1
+banksize $8000
+banks 1
+.endro
+
+.bank 0 slot 0
+.org $0000
+
+    nop
+    halt
+```
+
+### Create `linkfile_test`:
+```
+[objects]
+test.o
+
+[output]
+test.sms
